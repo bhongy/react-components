2a591730a9a729d20b68d28aeb1b3454e53afe82
Thanik Bhongbhibhat <bhongbhibhat@gmail.com>
2017-07-13 16:57:13 -0700

    Handle `onChange`

--- src/numeric-input.js
+++ src/numeric-input.js
@@ -1,23 +1,24 @@
 // @flow
 
 import React, { Component } from 'react';
+import { invoke } from 'lodash';
 
 type Value = number;
 
 type Props = {
   initialValue?: Value,
-  // onChange: (value: Value, name?: string) => void,
+  onChange?: (obj: { value: Value, name?: string }) => void,
   // precision: number,
 };
 
-type State = {
+export type State = {
   value: Value | null,
   inputValue: string,
 };
 
 function handleInitialValue(value?: Value): State {
   // `Number.isFinite` is better but no pre-edge IE support
-  // Flow: how to avoid checking type and use only `Number.isFinite` ?
+  // ? how to avoid checking type and use only `Number.isFinite` with Flow ?
   if (typeof value === 'number' && isFinite(value)) {
     return {
       value,
@@ -39,8 +40,35 @@ class NumericInput extends Component<void, Props, State> {
     this.state = handleInitialValue(props.initialValue);
   }
 
+  handleChange = (event: Event & { currentTarget: HTMLInputElement }) => {
+    // create a copy of "name" value because React synthetic event is re-used
+    // hence we cannot rely on the reference like `event.currentTarget.name`
+    const {
+      name,
+      value: inputValue,
+    // $FlowFixMe: how to use Flow with destructuring correctly
+    }: { value: string, name?: string } = event.currentTarget;
+    const numericValue: Value = +inputValue;
+
+    if (isNaN(numericValue)) {
+      return;
+    }
+
+    this.setState({ value: numericValue, inputValue }, () => {
+      // TODO: figure out how to fix Flow uncovered code here
+      // prefer to avoid ad-hoc null coalescing function
+      invoke(this.props, 'onChange', { value: numericValue, name })
+    });
+  };
+
   render() {
-    return <input value={this.state.inputValue} />;
+    return (
+      <input
+        {...this.props}
+        onChange={this.handleChange}
+        value={this.state.inputValue}
+      />
+    );
   }
 }
 
--- src/numeric-input.test.js
+++ src/numeric-input.test.js
@@ -3,7 +3,12 @@
 import React from 'react';
 import type { ShallowWrapper } from 'enzyme';
 import { shallow } from 'enzyme';
+import type { State } from './numeric-input';
 import NumericInput from './numeric-input';
+
+const createInputEvent = (value: string, name?: string) => ({
+  currentTarget: { name, value },
+});
 
 const expectResult = (rootWrapper: ShallowWrapper) =>
   expect({
@@ -62,6 +67,128 @@ describe('Components: Numeric Input', () => {
       expectResult(rendered).toEqual({
         state: 13.92,
         inputValue: '13.92',
+      });
+    });
+  });
+
+  describe('... handle input `onChange` to component state', () => {
+    let onChangeHandler;
+
+    beforeEach(() => {
+      onChangeHandler = jest.fn();
+      rendered = shallow(<NumericInput name="Gdańsk" onChange={onChangeHandler} />);
+    });
+
+    it('should call `props.onChange` when a numeric input value is provided', () => {
+      const inputName: string = rendered.find('input').prop('name');
+
+      rendered.simulate('change', createInputEvent('1921.856', inputName));
+
+      expect(onChangeHandler).toHaveBeenCalledTimes(1);
+      expect(onChangeHandler).toHaveBeenCalledWith({
+        name: 'Gdańsk',
+        value: 1921.856,
+      });
+    });
+
+    it('should ignore a non-numeric input value', () => {
+      const initialState: State = rendered.state();
+
+      rendered.simulate('change', createInputEvent('a'));
+
+      // do not create new state object
+      // use `toBe` to check identity
+      expect(rendered.state()).toBe(initialState);
+
+      // do not call the `onChange` handler
+      expect(onChangeHandler).toHaveBeenCalledTimes(0);
+    });
+
+    it('should not wipe the current numeric value when receiving a non-numeric input value', () => {
+      rendered.simulate('change', createInputEvent('01'));
+
+      expectResult(rendered).toEqual({
+        state: 1,
+        inputValue: '01',
+      });
+
+      expect(onChangeHandler).toHaveBeenCalledTimes(1);
+
+      const stateBeforeInvalidValue: State = rendered.state();
+
+      // type in a non-numeric value "a"
+      rendered.simulate('change', createInputEvent('01a'));
+
+      // the previous state is not wiped
+      expectResult(rendered).toEqual({
+        state: 1,
+        inputValue: '01',
+      });
+
+      // do not create a new state object
+      expect(rendered.state()).toBe(stateBeforeInvalidValue);
+
+      // do not call the `onChange` handler
+      expect(onChangeHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it('should allow one trailing period', () => {
+      rendered.simulate('change', createInputEvent('12.'));
+
+      expect(onChangeHandler).toHaveBeenCalledTimes(1);
+      expectResult(rendered).toEqual({
+        state: 12,
+        inputValue: '12.',
+      });
+    });
+
+    it('should allow numeric inputs after period', () => {
+      rendered.simulate('change', createInputEvent('83.100'));
+
+      expect(onChangeHandler).toHaveBeenCalledTimes(1);
+      expectResult(rendered).toEqual({
+        state: 83.1,
+        inputValue: '83.100',
+      });
+
+      rendered.simulate('change', createInputEvent('83.1000'));
+
+      expect(onChangeHandler).toHaveBeenCalledTimes(2);
+      expectResult(rendered).toEqual({
+        state: 83.1,
+        inputValue: '83.1000',
+      });
+    });
+
+    it('should ignore other period inputs after the first one', () => {
+      rendered.simulate('change', createInputEvent('72.'));
+
+      expectResult(rendered).toEqual({
+        state: 72,
+        inputValue: '72.',
+      });
+
+      rendered.simulate('change', createInputEvent('72..'));
+
+      // unchanged
+      expectResult(rendered).toEqual({
+        state: 72,
+        inputValue: '72.',
+      });
+
+      rendered.simulate('change', createInputEvent('72.1'));
+
+      expectResult(rendered).toEqual({
+        state: 72.1,
+        inputValue: '72.1',
+      });
+
+      rendered.simulate('change', createInputEvent('72.1.0'));
+
+      // unchanged
+      expectResult(rendered).toEqual({
+        state: 72.1,
+        inputValue: '72.1',
       });
     });
   });
