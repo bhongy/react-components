1da8d55882127edfaecf8e7aa4869a56bcf73459
Thanik Bhongbhibhat <bhongbhibhat@gmail.com>
2017-07-13 12:27:33 -0700

    Handle initial value
    Signed-off-by: Thanik Bhongbhibhat <bhongbhibhat@gmail.com>

--- src/numeric-input.js
+++ src/numeric-input.js
@@ -1,8 +1,46 @@
+// @flow
+
 import React, { Component } from 'react';
 
-class NumericInput extends Component {
+type Value = number;
+
+type Props = {
+  initialValue?: Value,
+  // onChange: (value: Value, name?: string) => void,
+  // precision: number,
+};
+
+type State = {
+  value: Value | null,
+  inputValue: string,
+};
+
+function handleInitialValue(value?: Value): State {
+  // `Number.isFinite` is better but no pre-edge IE support
+  // Flow: how to avoid checking type and use only `Number.isFinite` ?
+  if (typeof value === 'number' && isFinite(value)) {
+    return {
+      value,
+      inputValue: value.toString(),
+    };
+  }
+
+  return {
+    value: null,
+    inputValue: '',
+  };
+}
+
+class NumericInput extends Component<void, Props, State> {
+  state: State; // don't know why I need to do this
+
+  constructor(props: Props) {
+    super(props);
+    this.state = handleInitialValue(props.initialValue);
+  }
+
   render() {
-    return <input />;
+    return <input value={this.state.inputValue} />;
   }
 }
 
--- src/numeric-input.test.js
+++ src/numeric-input.test.js
@@ -1,6 +1,15 @@
+// @flow
+
 import React from 'react';
+import type { ShallowWrapper } from 'enzyme';
 import { shallow } from 'enzyme';
 import NumericInput from './numeric-input';
+
+const expectResult = (rootWrapper: ShallowWrapper) =>
+  expect({
+    state: rootWrapper.state('value'),
+    inputValue: rootWrapper.find('input').prop('value'),
+  });
 
 describe('Components: Numeric Input', () => {
   let rendered;
@@ -12,5 +21,48 @@ describe('Components: Numeric Input', () => {
   // design as a thin wrapper around HTMLInputElement
   it('should export Numeric Input with "input" component', () => {
     expect(rendered.name()).toBe('input');
+  });
+
+  describe('... handle initialValue to component state', () => {
+    it('should treat non-numeric values as if `initialValue` is not provided', () => {
+      [
+        undefined,
+        null,
+        { a: 'foo' },
+        ['1'],
+        '',
+        'bar',
+        true,
+        false,
+      ].forEach((value) => {
+        // $FlowExpectError
+        rendered = shallow(<NumericInput initialValue={value} />);
+
+        expectResult(rendered).toEqual({
+          state: null,
+          inputValue: '',
+        });
+      });
+    });
+
+    it('should treat NaN and Infinity as if `initialValue` is not provided', () => {
+      [NaN, Infinity, -Infinity].forEach((value) => {
+        rendered = shallow(<NumericInput initialValue={value} />);
+
+        expectResult(rendered).toEqual({
+          state: null,
+          inputValue: '',
+        });
+      });
+    });
+
+    it('should take a number and pass it to the input as a string', () => {
+      rendered = shallow(<NumericInput initialValue={13.92} />);
+
+      expectResult(rendered).toEqual({
+        state: 13.92,
+        inputValue: '13.92',
+      });
+    });
   });
 });
