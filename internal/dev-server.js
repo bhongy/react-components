d03b61dc8cbdd58f8c1e85d3c4b15841830b7945
Thanik Bhongbhibhat <bhongbhibhat@gmail.com>
2017-09-17 10:52:36 -0700

    Ensure Webpack devServer always serve "build/index.html"
    - fix error when refresh a page besides the root URL

--- internal/dev-server.js
+++ internal/dev-server.js
@@ -5,23 +5,23 @@ const webpackConfig = require('../config/webpack')({ production: false });
 
 const app = express();
 const compiler = webpack(webpackConfig);
-
-app.use(require('webpack-dev-middleware')(compiler, {
+const devMiddleware = require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: webpackConfig.output.publicPath,
   stats: {
     colors: true,
   },
-}));
+});
+const hotMiddleware = require('webpack-hot-middleware')(compiler);
 
-app.use(require('webpack-hot-middleware')(compiler));
-
+app.use(devMiddleware);
+app.use(hotMiddleware);
 app.get('*', (req, res) => {
-  res.sendFile(project.paths.build('index.html'));
+  const indexFile = devMiddleware.fileSystem.readFileSync(project.paths.build('index.html'));
+  res.end(indexFile);
 });
 
 const port = 8888;
-
 app.listen(port, (err) => {
   if (err) {
     return console.log(err);
