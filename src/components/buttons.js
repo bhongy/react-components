a1843f1742934a2239cfbac94916c1e1fe6e5d8a
Thanik Bhongbhibhat <bhongbhibhat@gmail.com>
2017-09-28 14:53:49 -0700

    Add uncovered test cases in `buttons.js`

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -4,7 +4,7 @@ import React from 'react';
 import cx from 'classnames';
 import s from './buttons.css';
 
-export const RaisedButton = ({ className, disabled, ...otherProps }) => {
+const RaisedButton = ({ className, disabled, ...otherProps }) => {
   const baseClassName = disabled ? s.raised__disabled : s.raised__active;
   return (
     <button
@@ -15,7 +15,12 @@ export const RaisedButton = ({ className, disabled, ...otherProps }) => {
   );
 };
 
-export const FlatButton = ({ className, disabled, ...otherProps }) => {
+// needs to provide displayName to avoid "Unknown" stateless component names
+// when running --coverage (issue with istanbul)
+// https://github.com/facebook/jest/issues/1824
+RaisedButton.displayName = 'RaisedButton';
+
+const FlatButton = ({ className, disabled, ...otherProps }) => {
   const baseClassName = disabled ? s.flat__disabled : s.flat;
   return (
     <button
@@ -25,3 +30,7 @@ export const FlatButton = ({ className, disabled, ...otherProps }) => {
     />
   );
 };
+
+FlatButton.displayName = 'FlatButton';
+
+export { FlatButton, RaisedButton };
--- src/components/buttons.test.js
+++ src/components/buttons.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { RaisedButton } from './buttons';
+import { FlatButton, RaisedButton } from './buttons';
+
+describe('Flat Button', () => {
+  it('renders button', () => {
+    expect(shallow(<FlatButton />).type()).toBe('button');
+  });
+
+  it('renders disabled button with correct props', () => {
+    expect(shallow(<FlatButton disabled />)).toMatchSnapshot();
+  });
+});
 
 describe('Raised Button', () => {
   it('renders button', () => {
     expect(shallow(<RaisedButton />).type()).toBe('button');
+  });
+
+  it('renders disabled button with correct props', () => {
+    expect(shallow(<RaisedButton disabled />)).toMatchSnapshot();
   });
 });
