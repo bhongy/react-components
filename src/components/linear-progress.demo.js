74c8452f128fac0230b6aa020c06944720ef918c
Thanik Bhongbhibhat <thanikb@pinterest.com>
2017-12-02 17:37:20 -0800

    Demo with load button works
    Signed-off-by: Thanik Bhongbhibhat <thanikb@pinterest.com>

--- src/components/linear-progress.demo.js
+++ src/components/linear-progress.demo.js
@@ -5,6 +5,63 @@ import s from './linear-progress.demo.css';
 
 const Card = ({ children }) => <div className={s.card}>{children}</div>;
 
+class SimpleUseCaseDemo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.intervalId = null;
+    this.state = { isLoading: false, value: 0 };
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  startLoading = () => {
+    this.setState({ isLoading: true, value: 0 });
+
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+    this.intervalId = setInterval(() => {
+      this.setState(prevState => {
+        const { completeAt } = this.props;
+        const delta = Math.round(Math.random() * (completeAt / 4));
+        const nextValue = prevState.value + delta;
+        if (nextValue < completeAt) {
+          return { value: nextValue };
+        }
+        return { value: 300, isLoading: false };
+      });
+    }, 600);
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.isLoading && !this.state.isLoading) {
+      clearInterval(this.intervalId);
+      console.log('Done!');
+      // this.props.onLoadingDidEnd();
+    }
+  }
+
+  render() {
+    const { completeAt } = this.props;
+    const { value } = this.state;
+    return (
+      <Fragment>
+        <Card>
+          <header className={s.header}>
+            <LinearProgress percentCompleted={100 * value / completeAt} />
+          </header>
+        </Card>
+        <Button className={s.button} onClick={this.startLoading}>
+          Load
+        </Button>
+      </Fragment>
+    );
+  }
+}
+
 class LinearProgressDemo extends React.PureComponent {
   state = { v: 160 };
 
@@ -19,12 +76,7 @@ class LinearProgressDemo extends React.PureComponent {
       <div>
         <h3>LinearProgress</h3>
         <div className={s.demosContainer}>
-          <Card>
-            <header className={s.header}>
-              <LinearProgress percentCompleted={60} />
-            </header>
-          </Card>
-          <Button className={s.button}>Load</Button>
+          <SimpleUseCaseDemo completeAt={300} />
           <Card>
             <header className={s.header}>
               <LinearProgress percentCompleted={this.state.v} />
