c99fb4a51eb5e3e1d3ea4b25dd5bb39a7ea4ed58
Thanik Bhongbhibhat <thanikb@pinterest.com>
2017-12-02 16:15:14 -0800

    Add 2 demo variations: out-of-range handling (+PropType) and bottom aligned
    Signed-off-by: Thanik Bhongbhibhat <thanikb@pinterest.com>

--- src/components/linear-progress.demo.js
+++ src/components/linear-progress.demo.js
@@ -3,30 +3,39 @@ import LinearProgress from './linear-progress';
 import { FlatButton as Button } from './buttons';
 import s from './linear-progress.demo.css';
 
-const Card = ({ children }) => (
-  <div className={s.card}>{children}</div>
-);
-
-const Demo = ({ children }) => (
-  <Fragment>
-    <Card>
-      <header className={s.header}>
-        <LinearProgress percentCompleted={60} />
-      </header>
-      {children}
-    </Card>
-    <Button>Load</Button>
-  </Fragment>
-);
+const Card = ({ children }) => <div className={s.card}>{children}</div>;
 
 class LinearProgressDemo extends React.PureComponent {
-  state = {};
+  state = { v: 160 };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ v: 230 });
+    }, 1000);
+  }
+
   render() {
     return (
       <div>
         <h3>LinearProgress</h3>
         <div className={s.demosContainer}>
-          <Demo />
+          <Card>
+            <header className={s.header}>
+              <LinearProgress percentCompleted={60} />
+            </header>
+          </Card>
+          <Button className={s.button}>Load</Button>
+          <Card>
+            <header className={s.header}>
+              <LinearProgress percentCompleted={this.state.v} />
+            </header>
+            <p>Invalid proptype.</p>
+          </Card>
+          <Card>
+            <footer className={s.footer}>
+              <LinearProgress percentCompleted={35} />
+            </footer>
+          </Card>
         </div>
       </div>
     );
--- src/components/linear-progress.js
+++ src/components/linear-progress.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import { once } from 'lodash';
 // import { Motion, spring } from 'react-motion';
 import s from './linear-progress.css';
 
@@ -11,12 +12,34 @@ import s from './linear-progress.css';
 // }
 
 type Props = {
+  // choose to use `percentCompleted` rather than `value` and `max`
+  // so that when the `percentCompleted` is out of [0, 100] range
+  // we know it's the logic that calculated percentage outside
+  // of this component (more explicit, less magical)
+  // hence by design - this component is dumb and takes value [0, 100]
   percentCompleted: number,
   height?: number,
 };
 
+const PropTypes = {
+  inRange(min, max) {
+    // this will log the warning once for this component
+    // so we don't flood your console with the same proptype warnings
+    const warn = once((propName, value) =>
+      new Error(`"${propName}". Expect between [${min}, ${max}] but received: ${value}`));
+
+    return (props, propName) => {
+      const v = props[propName];
+      // TODO: this is [min, max] but generally people expect [min, max) (exclude max)
+      if (v < min || v > max) {
+        return warn(propName, v);
+      }
+    };
+  },
+};
+
 class LinearProgress extends React.PureComponent<Props, {}> {
-  // static PropTypes = {};
+  static propTypes = { percentCompleted: PropTypes.inRange(0, 100) };
   static defaultProps = {
     height: 4,
   };
@@ -24,7 +47,8 @@ class LinearProgress extends React.PureComponent<Props, {}> {
   state = {};
 
   render() {
-    const { height, percentCompleted } = this.props;
+    const { height } = this.props;
+    const percentCompleted = Math.max(0, Math.min(100, this.props.percentCompleted));
     const width = `${percentCompleted}%`;
     return (
       <div className={s.container}>
